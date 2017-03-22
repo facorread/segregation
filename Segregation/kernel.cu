a2b576ea7c694a77293219933c95c2d920224aeb
Fabio Correa (Win) <facorread@gmail.com>
2017-03-22 02:28:07 -0400

    Sync, random

--- Segregation/kernel.cu
+++ Segregation/kernel.cu
@@ -17,13 +17,15 @@ along with SchellingSegregation.  If not, see <http://www.gnu.org/licenses/>.
 #include <algorithm>
 #include <cuda_runtime.h>
 #include "cudaGrid.h"
+#include <curand_kernel.h>
 #include <device_launch_parameters.h>
 #include <thrust/device_vector.h>
 #include <thrust/host_vector.h>
 
 namespace cudaSim {
 constexpr int nSigmaValues{100};
-typedef dimensions2Cls<dimCls<unsigned char, 50>, dimCls<unsigned char, 50>> gridT;
+constexpr int threadsPerBlock{256};
+typedef dimensions2Cls<dimCls<unsigned char, 64>, dimCls<unsigned char, 64>> gridT;
 
 /// Represents the status of a cell.
 enum class cellStatusEnum : char {
@@ -41,14 +43,24 @@ struct __align__(4) cellCls {
 /// Represents variables and components to the simulation that reside in shared memory.
 struct worldCls {
 	float sigma;
+	curandStatePhilox4_32_10_t randStates[threadsPerBlock];
 	cellCls states[gridT::size()];
 };
 
 __global__ void kernel(float nNeighborsSigma[]) {
 	__shared__ worldCls world;
-	world.sigma = blockIdx.x / static_cast<float>(nSigmaValues);
+	if(!threadIdx.x) {
+		world.sigma = blockIdx.x / static_cast<float>(nSigmaValues);
+	}
+	const unsigned long long thx2{blockIdx.x * threadIdx.x + blockIdx.x + threadIdx.x + 6};
+	curand_init(5 * thx2, 2 * thx2, 3 * thx2, &world.randStates[threadIdx.x]);
+	__syncthreads();
+	for(unsigned short i{threadIdx.x}; i < gridT::size(); i += threadsPerBlock) {
+		world.states[i].nAffineNeighbors = threadIdx.x;
+	}
+	__syncthreads();
 	if(!threadIdx.x)
-		nNeighborsSigma[blockIdx.x] = sizeof(world.states);
+		nNeighborsSigma[blockIdx.x] = curand(&world.randStates[threadIdx.x]);
 }
 } // namespace cudaSim
 
@@ -56,7 +68,7 @@ namespace sim {
 void main() {
 	thrust::device_vector<float> nNeighborsSigma;
 	nNeighborsSigma.resize(cudaSim::nSigmaValues, 0);
-	cudaSim::kernel<<<cudaSim::nSigmaValues, 32>>>(thrust::raw_pointer_cast(nNeighborsSigma.data()));
+	cudaSim::kernel<<<cudaSim::nSigmaValues, cudaSim::threadsPerBlock >>>(thrust::raw_pointer_cast(nNeighborsSigma.data()));
 	const thrust::host_vector<float> nNeighborsSigmaDevice{nNeighborsSigma};
 	int sigmaI{0};
 	for(const float nNeighSigma : nNeighborsSigmaDevice)
